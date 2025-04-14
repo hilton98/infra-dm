8249428fc58d7d895f012710bd50d4aa04325f16
hilton98 <hscn@icomp.ufam.edu.br>
2025-04-14 15:16:01 -0400

    apply policy for actions s3

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -9,14 +9,15 @@ resource "aws_s3_bucket" "static_app_bucket" {
 
 resource "aws_s3_bucket_public_access_block" "static_app_bucket" {
   bucket = aws_s3_bucket.static_app_bucket.id
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_ownership_controls" "static_app_bucket" {
   bucket = aws_s3_bucket.static_app_bucket.id
+
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
@@ -29,5 +30,25 @@ resource "aws_s3_bucket_acl" "static_app_bucket" {
   ]
 
   bucket = aws_s3_bucket.static_app_bucket.id
-  acl = "public-read"
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "allow_all_valid_iam" {
+  bucket = aws_s3_bucket.static_app_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowAllActionsForAllUsersWithValidIAM"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          "arn:aws:s3:::static-app-${var.bucket_name}",
+          "arn:aws:s3:::static-app-${var.bucket_name}/*"
+        ]
+      }
+    ]
+  })
 }
