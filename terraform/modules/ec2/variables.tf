ab91b1b76ff051ff178f2c0962e4e93ff79949fe
hilton98 <hscn@icomp.ufam.edu.br>
2025-04-15 11:11:26 -0400

    removes variables not used

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -2,18 +2,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "ami" {
-  type = string
-}
-
-variable "subnet_id" {
-  type = string
-}
-
-variable "vpc_security_group_id" {
-  type = string
-}
-
 variable "key_name" {
   type = string
 }
