355f7c532539e1c63e5ec9670f06f6e7096a4d7b
hilton98 <hscn@icomp.ufam.edu.br>
2025-04-15 13:11:32 -0400

    Config system

--- terraform/scripts/deploy-api.sh
+++ terraform/scripts/deploy-api.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
-if [ ! -d "/home/ubuntu/app" ]; then
-  echo "Directory /home/ubuntu/app not found. Cloning repository..."
-  git clone https://github.com/hilton98/project-teste.git /home/ubuntu/app
+echo "Update System..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing Node..."
+sudo apt-get install -y curl
+
+curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+
+sudo -E bash nodesource_setup.sh
+
+sudo apt-get install -y nodejs
+
+node -v
+
+APP_DIR="/home/ubuntu/app"
+
+if [ ! -d "$APP_DIR" ]; then
+  echo "Directory $APP_DIR not found. Cloning repository..."
+  git clone https://github.com/hilton98/project-teste.git "$APP_DIR"
 else
-  echo "Directory /home/ubuntu/app found. Update repository..."
-  cd /home/ubuntu/app
+  echo "Directory $APP_DIR found. Update repository..."
+  cd "$APP_DIR"
   git pull origin main
 fi
 
-cd /home/ubuntu/app
+cd "$APP_DIR"
 
-echo "Instaling dependencies..."
+echo "Instaling API dependencies..."
 npm install
 
-echo "Build api..."
+echo "Build API..."
 npm run build
 
-echo "Instaling PM2..."
+echo "Installing PM2..."
 sudo npm install -g pm2
 
-echo "Starting/Restart api with PM2..."
+echo "Start/Restart API with PM2..."
 pm2 start dist/main.js --name dm-api || pm2 restart dm-api
